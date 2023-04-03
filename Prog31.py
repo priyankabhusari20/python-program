22ffa664ca0252cc367cd6ee5804a8bf3f3e01a7
priyankabhusari20 <120464894+priyankabhusari20@users.noreply.github.com>
2023-04-03 22:54:41 +0530

    Add files via upload

---
+++ Prog31.py
@@ -0,0 +1,13 @@
+# Character operations using for Loop
+
+city="Amravati"
+for ch in city:
+    print(ch)
+
+# Finding a in Character
+
+count=0
+for ch in city:
+    if ch=="a" or ch=="A":
+        count+=1
+print(count)
